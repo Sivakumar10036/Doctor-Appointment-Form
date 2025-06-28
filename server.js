3c013f84312f822056202f09f59837f0d14a5577
Sivakumar10036 <rsiva19022005@gmail.com>
2025-06-28 11:12:43 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import Form from "./components/Form"; // Correct path
+
+function App() {
+  return (
+    <div>
+      <h1>Doctor-Appointment-Form</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
